11f0da37ee3398e4bc324d68d0a26d78539b14bf
kalin <kalindusupun@gmail.com>
2021-08-15 19:18:18 +0530

    Chat App Upload

---
+++ app/src/main/java/com/chatapp/chats_adapter/FirebaseAllChatsAdapter.java
@@ -0,0 +1,83 @@
+package com.chatapp.chats_adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.chatapp.R;
+import com.chatapp.SpecificChat;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.squareup.picasso.Picasso;
+
+public class FirebaseAllChatsAdapter extends FirestoreRecyclerAdapter<ChatsModel,FirebaseAllChatsAdapter.ChatsViewHolder> {
+
+    Context context;
+    FirestoreRecyclerOptions<ChatsModel> fireStoreRecyclerOptions;
+
+    public FirebaseAllChatsAdapter(@NonNull Context context, FirestoreRecyclerOptions<ChatsModel> fireStoreRecyclerOptions) {
+        super(fireStoreRecyclerOptions);
+        this.context = context;
+        this.fireStoreRecyclerOptions = fireStoreRecyclerOptions;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull FirebaseAllChatsAdapter.ChatsViewHolder holder, int position, @NonNull ChatsModel model) {
+
+        holder.particularusername.setText(model.getName());
+        String uri=model.getImage();
+
+        Picasso.get().load(uri).into(holder.mimageviewofuser);
+        if(model.getStatus().equals("Online"))
+        {
+            holder.statusofuser.setText(model.getStatus());
+            holder.statusofuser.setTextColor(Color.GREEN);
+        }
+        else
+        {
+            holder.statusofuser.setText(model.getStatus());
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(context, SpecificChat.class);
+                intent.putExtra("name",model.getName());
+                intent.putExtra("receiveruid",model.getUid());
+                intent.putExtra("imageuri",model.getImage());
+                context.startActivity(intent);
+            }
+        });
+
+    }
+
+    @NonNull
+    @Override
+    public ChatsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.current_chat_item_layout,parent,false);
+        return new ChatsViewHolder(view);
+    }
+
+    public class ChatsViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView particularusername;
+        private TextView statusofuser;
+        private ImageView mimageviewofuser;
+
+        public ChatsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            particularusername=itemView.findViewById(R.id.nameofuser);
+            statusofuser=itemView.findViewById(R.id.statusofuser);
+            mimageviewofuser=itemView.findViewById(R.id.imageviewofuser);
+        }
+    }
+}
--- app/src/main/java/com/chatapp/fragments/ChatFragment.java
+++ app/src/main/java/com/chatapp/fragments/ChatFragment.java
@@ -4,14 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.chatapp.R;
+import com.chatapp.chats_adapter.ChatsModel;
+import com.chatapp.chats_adapter.FirebaseAllChatsAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class ChatFragment extends Fragment {
+    private FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    FirebaseUser firebaseUser;
+
+    RecyclerView recyclerViewChat;
+    FirebaseAllChatsAdapter allChatsAdapter;
 
     @Nullable
     @Override
@@ -19,7 +37,46 @@ public class ChatFragment extends Fragment {
 
         View v =inflater.inflate(R.layout.fragment_call,null);
 
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        recyclerViewChat = v.findViewById(R.id.recyclerViewChat);
+
+        syncDataFromFirebase();
+
         return v;
+    }
+
+    public void syncDataFromFirebase(){
+        Query query = firebaseFirestore.collection("Users");
+
+        FirestoreRecyclerOptions<ChatsModel> allChats = new FirestoreRecyclerOptions.Builder<ChatsModel>().setQuery(query, ChatsModel.class).build();
+
+        allChatsAdapter = new FirebaseAllChatsAdapter(getContext(),allChats);
+        recyclerViewChat.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager =new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerViewChat.setLayoutManager(linearLayoutManager);
+        recyclerViewChat.setAdapter(allChatsAdapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        allChatsAdapter.startListening();
+        recyclerViewChat.setAdapter(allChatsAdapter);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if(allChatsAdapter!=null)
+        {
+            allChatsAdapter.stopListening();
+            //noteAdapter.startListening();
+        }
     }
 
 }
