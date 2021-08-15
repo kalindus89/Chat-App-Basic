77256371ec712220f0687806b44427bd227e4443
kalin <kalindusupun@gmail.com>
2021-08-15 11:00:57 +0530

    Chat App Upload

---
+++ app/src/main/java/com/chatapp/login_signup/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.chatapp.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chatapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/chatapp/login_signup/OtpAuthentication.java
@@ -0,0 +1,18 @@
+package com.chatapp.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chatapp.R;
+
+public class OtpAuthentication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_authentication);
+
+
+    }
+}
